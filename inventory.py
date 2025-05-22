58379a035e33cc611f697640f7340f93d6eb220e
isiolilyGit <lilyime272@gmail.com>
2025-05-23 00:11:18 +0300

    Added a function to update inventory

--- inventory.py
+++ inventory.py
@@ -12,6 +12,24 @@ def add_item(name, quantity, price):
         items[name]["quantity"] += quantity
         items[name]["price"] = price
 
+#A function to update items and their details
+def update_item():
+    name = input("Enter the item's name to be updated ")
+    if name in items:
+        new_quantity = int(input(f"Enter the new quantity for {name} "))
+        new_price = float(input(f"Enter the new price {name} "))
+        if new_quantity:
+            items[name]["quantity"] = int(new_quantity)
+        if new_price:
+            items[name]["price"] = float(new_price)
+
+        print("The updated list\n")
+        for name, details in items.items():
+            print(f"{details['quantity']} {name}s cost {details['price']:.2f}")
+             
+    else:
+        print(f"Item {name} not found")
+
 #A function to display the items
 def display_items():
     if not items:
@@ -31,6 +49,7 @@ def main():
         print("2. Remove items from the list")
         print("3. Update items of the list")
         print("4. Display items of the list")
+        print("5. Exit")
 
         choice = input("Enter a choice from the menu\n")
 
@@ -40,7 +59,17 @@ def main():
             price = float(input("Enter the price "))
             add_item(name, quantity, price)
         
+        elif choice == "3":
+            update_item()
+
         elif choice == "4":
             display_items()
+        
+        elif choice == "5":
+            break
+
+        else:
+            print("Invalid choice")
+
 
 main()
