0334312570d9666d324288dc7343c367c3ccfcd4
isiolilyGit <lilyime272@gmail.com>
2025-05-22 19:22:17 +0300

    Added a function to add items and a function to display items

--- inventory.py
+++ inventory.py
@@ -2,6 +2,7 @@
 
 items = {}
 
+#A function to add items to the dictionary
 def add_item(name, quantity, price):
     if name not in items:
         items[name] = {"quantity":quantity, "price":price}
@@ -10,3 +11,36 @@ def add_item(name, quantity, price):
     else:
         items[name]["quantity"] += quantity
         items[name]["price"] = price
+
+#A function to display the items
+def display_items():
+    if not items:
+        print("There are no items to display")
+    
+    else:
+        for name, details in items.items():
+            print(f"{details['quantity']} {name}s cost {details['price']:.2f}")
+        
+
+def main():
+    #Prompting the user to make a choice
+    #to add or remove or display or update items
+    while True:
+        print("Inventory menu")
+        print("1. Add items to the list")
+        print("2. Remove items from the list")
+        print("3. Update items of the list")
+        print("4. Display items of the list")
+
+        choice = input("Enter a choice from the menu\n")
+
+        if choice == "1":
+            name = input("Enter the item's name ")
+            quantity = int(input("How many of the items do you want "))
+            price = float(input("Enter the price "))
+            add_item(name, quantity, price)
+        
+        elif choice == "4":
+            display_items()
+
+main()
